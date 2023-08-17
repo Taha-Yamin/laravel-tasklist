3bf47202cda6a2179584705ff7e9da993860e68f
Taha Yamin <taha.yamin20@gmail.com>
2023-08-17 06:41:23 +0500

    Project Completed

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,17 +1,43 @@
 @extends('layouts.app')
 @section('title')
-    <h1>{{$task->title}}</h1>
+    {{$task->title}}
 @endsection
 
 
 @section('content')
-    <p>{{$task->description}}</p>
+    <p >{{$task->description}}</p>
 
     @isset($task->long_description)
     <p>{{$task->long_description}}</p>
     @endisset
-    <p>{{$task->completed}}</p>
+    <p>{{$task->completed}}
+        @if($task->completed)
+        Completed
+        @else
+        Not Completed
+        @endif
+    </p>
+
     <p>{{$task->created_at}}</p>
     <p>{{$task->updated_at}}</p>
+    <a href="{{ route('tasks.edit',['task'=>$task->id]) }}"><button>Edit</button></a>
+    <form Method="POST" action="{{ route('tasks.toggle-complete',['task'=> $task->id] )}}">
+        @csrf
+        @method('PUT')
+        <button type="submit">
+            @if($task->completed)
+            Mark as Not Completed
+            @else
+            Mark as Completed
+            @endif
+        </button>
+    </form>
+    <form action="{{ route('tasks.destroy',['task'=>$task->id]) }}" method="POST">
+        @csrf
+        @method('delete')
+        
+        <button type="submit">Delete</button>
+    </form>
+    
 @endsection
   
